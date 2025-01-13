0a6264ce53d18093d86a15d4f0727fa0123d220f
= <=>
2025-01-13 21:48:39 +0300

    feat: private page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,25 @@
+import { unsubscribe } from "diagnostics_channel";
+import { Auth } from "firebase-admin/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Dashboard(){
+    const router = useRouter();
+    const auth = getAuth();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if(!user){
+                router.push("./page");
+            }
+        });
+        return  () => unsubscribe();
+    }, [auth, router]);
+    
+    return (
+        <div>
+            <p>Bienvenue, ceci est une page privée.</p>
+        </div>
+    )
+}
